baf5ea735796fe2f9bb041ca659c7452b5007fab
Caio Collete <caiocolletesilva@gmail.com>
2023-08-19 11:58:24 -0300

    add search show in console log

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggle(){
     const html = document.documentElement
     html.classList.toggle("light")
 }
+
+function search() {
+    const input = document.getElementById("buscaInput");
+
+    console.log(input.value);
+}
+
+const button = document.getElementById("buscaBtn");
+
+button.addEventListener("click", search);
